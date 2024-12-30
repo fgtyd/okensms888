0fe7309b69a18551848a159e97e99dc039444542
fgtyd <gstgusuqgjdfs7@proton.me>
2024-12-30 06:11:15 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,22 +7,22 @@ fi
 echo ${VER}
 
 if [[ -z "${Vless_Path}" ]]; then
-  Vless_Path="/394fe03d-289a-4dea-b9b5-0bed468260f3"
+  Vless_Path="/4d2ae90a-613e-497e-9af6-195eb0af056c"
 fi
 echo ${Vless_Path}
 
 if [[ -z "${Vless_UUID}" ]]; then
-  Vless_UUID="394fe03d-289a-4dea-b9b5-0bed468260f3"
+  Vless_UUID="4d2ae90a-613e-497e-9af6-195eb0af056c"
 fi
 echo ${Vless_UUID}
 
 if [[ -z "${Vmess_Path}" ]]; then
-  Vmess_Path="/5d293e61-305e-4003-ac87-4587b36646aa"
+  Vmess_Path="/ee29b315-19e2-4de1-aeb8-9e20d19e16cd"
 fi
 echo ${Vmess_Path}
 
 if [[ -z "${Vmess_UUID}" ]]; then
-  Vmess_UUID="5d293e61-305e-4003-ac87-4587b36646aa"
+  Vmess_UUID="ee29b315-19e2-4de1-aeb8-9e20d19e16cd"
 fi
 echo ${Vmess_UUID}
 
